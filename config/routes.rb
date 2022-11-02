66d4a290b58560dbf9ad2a498d81a0be0cfc3eaf
Abby Nyakara <abbynyakara@gmail.com>
2022-11-02 16:53:05 +0300

    set-up entities and groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :entities
+  resources :groups
+  devise_for :users
+
+  root "groups#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
